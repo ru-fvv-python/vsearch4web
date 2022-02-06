3aeebb1fc9d48165a2af02c4ab3c52c1e88cbc06
vlad-fvv <vladfomin2008@gmail.com>
2022-02-06 15:03:11 +0300

    Функция записи данных в журнал теперь выполняется в параллельном потоке.

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,7 +1,11 @@
 
 from flask import Flask, render_template, request, session
+from flask import copy_current_request_context
 from vsearch import search4letters
 from checker import check_logged_in
+from time import sleep
+from threading import Thread
+
 
 # диспетчер контекста
 from DBcm import UseDataBase, ConnectionError, CredentialError, SQLError
@@ -32,44 +36,54 @@ def do_logout() -> str:
     return 'You are now logged out'
 
 
-def log_request(req: 'flask_request', res: str) -> None:
-    """Журналирование запросов и результатов обаботки в 
-    базе данных MySQL""" 
-    # используем диспетчер контекста
-    try:
-        with UseDataBase(app.config['dbconfig']) as cursor:
-            # строка с текстом запроса
-            _SQL = """insert into log
-                    (phrase, letters, ip, browser_string, results)
-                    values
-                    (%s, %s, %s, %s, %s)"""
-
-            # запуск запроса. параметры: данные из HTML-формы веб-приложения
-            cursor.execute(_SQL, (req.form['phrase'],
-                                req.form['letters'],
-                                req.remote_addr,
-                                req.user_agent.browser,
-                                res,))
-    except ConnectionError as err:
-        print('Is your database switched on? Error:', str(err))
-    except CredentialError as err:
-        print('User-id/Password issues. Error:', str(err))
-    except SQLError as err:
-        print('Is your query correct? Error:', str(err))
-    except Exception as err:
-        print('Something went wrong:', str(err))
-
-        
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     """ Считывание фразы и символов из полей формы
-        Передача их функции search4letters
-        Журналирование формирование формы с результатом: сохранение в БД"""
+    Передача их функции search4letters
+    Журналирование формирование формы с результатом: сохранение в БД"""
+
+    """Декоратор copy_current_request_context гарантирует, что HTTP-запрос,
+    который активен в момент вызова функции, останется активным, когда функция
+    позже запустится в отдельном потоке."""
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        """Журналирование запросов и результатов обработки в
+        базе данных MySQL"""
+        # This makes log_request really slow...
+        sleep(15)
+        try:
+            # используем диспетчер контекста
+            with UseDataBase(app.config['dbconfig']) as cursor:
+                # строка с текстом запроса
+                _SQL = """insert into log
+                        (phrase, letters, ip, browser_string, results)
+                        values
+                        (%s, %s, %s, %s, %s)"""
+
+                # запуск запроса. параметры: данные из HTML-формы веб-приложения
+                cursor.execute(_SQL, (req.form['phrase'],
+                                      req.form['letters'],
+                                      req.remote_addr,
+                                      req.user_agent.browser,
+                                      res,))
+        except ConnectionError as err:
+            print('Is your database switched on? Error:', str(err))
+        except CredentialError as err:
+            print('User-id/Password issues. Error:', str(err))
+        except SQLError as err:
+            print('Is your query correct? Error:', str(err))
+        except Exception as err:
+            print('Something went wrong:', str(err))
+        return 'Error'
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(search4letters(phrase, letters))
     title = 'Here are your results:'
-    log_request(request, results)
+    try:
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
+    except Exception as err:
+        print("***** Logging failed with this error:", str(err))
     return render_template('results.html',
                            the_title = title,
                            the_phrase = phrase,
